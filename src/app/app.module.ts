aea30f74ab26efe29232f9ae60e6745e341538f4
umutaktas <umutaktas@gmail.com>
2017-12-25 00:37:44 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
+import { RegisterComponent } from '../components/register/register.component';
+import { HomeComponent } from '../components/home/home.component';
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from '../components/login/login.component';
+import {AuthService} from '../services/auth.service';
+import {AuthGuardService} from '../services/auth-guard.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
+
+
+export const routeConfig: Routes =  [
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot(routeConfig),
   ],
-  providers: [],
+  providers: [AuthService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyA3jQ89a0WYcaOhcrqBTZcJr1Jxru0Uvck",
+    authDomain: "biyone-183206.firebaseapp.com",
+    databaseURL: "https://biyone-183206.firebaseio.com",
+    projectId: "biyone-183206",
+    storageBucket: "biyone-183206.appspot.com",
+    messagingSenderId: "79835270543"
+  }
 };
